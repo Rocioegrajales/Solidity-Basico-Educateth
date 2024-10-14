7223bae93f0dc29357c048be9eaca346f283cab3
Rocioegrajales <rocioegrajales@gmail.com>
2024-10-14 13:46:11 -0500

    Primer contrato

---
+++ HolaMundo.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+// A partir de aca coloco la logica de mi contrato
+contract HolaMundo {
+    string public saludo = "Hola desde Educateth";  // Declarando el contenido de mi variable saludo
+}
